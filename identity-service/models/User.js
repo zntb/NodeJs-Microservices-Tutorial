0881ce2885fcfac8a65bc7f4486c2b353cfdc963
zntb <zenetibi@yahoo.com>
2025-01-06 08:11:14 +0200

    Add User schema

---
+++ identity-service/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const argon2 = require('argon2');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    try {
+      this.password = await argon2.hash(this.password);
+    } catch (error) {
+      return next(error);
+    }
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await argon2.verify(this.password, candidatePassword);
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.index({ username: 'text' });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
