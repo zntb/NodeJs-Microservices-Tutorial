63c8f113bfaf1b22e662555af4be6ea909822bc2
zntb <zenetibi@yahoo.com>
2025-01-07 13:39:29 +0200

    Add Media model

---
+++ media-service/src/models/Media.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const mediaSchema = new mongoose.Schema(
+  {
+    publicId: {
+      type: String,
+      required: true,
+    },
+    originalName: {
+      type: String,
+      required: true,
+    },
+    mimeType: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Media = mongoose.model('Media', mediaSchema);
+
+module.exports = Media;
